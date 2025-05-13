3c8bf6c34d004e6b898482ef10153f6f31f6e3fd
sakurawen <wisakura@outlook.com>
2025-05-14 07:35:51 +0800

    feat: save

--- src/app/action.ts
+++ src/app/action.ts
@@ -2,6 +2,13 @@
 import ky from 'ky';
 
 export async function getEmail(values: { email: string, password: string, proxy?: string }) {
-  const res = await ky.get(values.proxy ? `http://120.78.1.125:8000/email/${values.password}/${values.email}?proxy=${values.proxy}` : `http://120.78.1.125:8000/email/${values.password}/${values.email}`).text();
+  const res = await ky.get(
+    values.proxy
+      ? `http://120.78.1.125:8000/email/${values.password}/${values.email}?proxy=${values.proxy}`
+      : `http://120.78.1.125:8000/email/${values.password}/${values.email}`,
+    {
+      timeout: 120000, // 2 minutes timeout
+    },
+  ).text();
   return res;
 }
